42230ff59965625103f024fc2a11dffcf1407f76
coleos-deploy <info@coenconstruction.com>
2026-09-11 22:53:13 +0000

    Stamp service-worker version

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  *     Icons, the PDF and the OG image are content-addressed by deploy, so serving
  *     them from cache instantly is safe.
  */
-const VERSION = "8b8c3ba45f86";
+const VERSION = "942f7f4ddfc8";
 const SHELL = "coleos-shell-" + VERSION;
 const ASSETS = "coleos-assets-" + VERSION;
 
